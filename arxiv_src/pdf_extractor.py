c58be7e58ef30fe8096ed0b6221c62f8966b68ef
91097luke <lukecamilleri97@gmail.com>
2023-03-14 16:16:03 +0000

    + pdf_extractor v2

--- arxiv_src/pdf_extractor.py
+++ arxiv_src/pdf_extractor.py
@@ -1,5 +1,6 @@
 from io import StringIO
 
+import pypdf.errors
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
@@ -8,76 +9,82 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
 import PyPDF2
+import string
 import csv
+import re
+from pypdf import PdfReader
 
 from pathlib import Path
 from tqdm import tqdm
+import os
+import json
+EOF_MARKER = b'%%EOF'
 
-def convert_pdf_to_string(file_path):
-    output_string = StringIO()
-    with open(file_path, 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-
-    return (output_string.getvalue())
-
-
-def convert_title_to_filename(title):
-    filename = title.lower()
-    filename = filename.replace(' ', '_')
-    return filename
-
-
-def split_to_title_and_pagenum(table_of_contents_entry):
-    title_and_pagenum = table_of_contents_entry.strip()
-
-    title = None
-    pagenum = None
-
-    if len(title_and_pagenum) > 0:
-        if title_and_pagenum[-1].isdigit():
-            i = -2
-            while title_and_pagenum[i].isdigit():
-                i -= 1
-
-            title = title_and_pagenum[:i].strip()
-            pagenum = int(title_and_pagenum[i:].strip())
-
-    return title, pagenum
 
 def extract_text(file_path):
-    text = ''
-    if file_path.endswith('.pdf'):
+    if str(file_path).endswith('.pdf'):
         # Extract text from a PDF file
-        with open(file_path, 'rb') as pdf_file:
-            pdf_reader = PyPDF2.PdfReader(pdf_file)
-            num_pages = len(pdf_reader.pages)
-            for i in range(num_pages):
-                page = pdf_reader.pages[i]
-                page_text = page.extract_text()
-                text += page_text
+        text = ''
+        try:
+            reader = PdfReader(str(file_path))
 
-        return text
+            number_of_pages = len(reader.pages)
+            for i in range(number_of_pages):
+                page = reader.pages[i]
+                page_text = page.extract_text()
+                text += page_text + "\n"
+
+            return text
+
+        except pypdf.errors.PyPdfError:
+            print(f'could not load: {file_path}')
+            return -1
 
     else:
         print('Unsupported file type')
 
+
+def filter_nonprintable_characters(text):
+    printable_text = ''.join(filter(lambda x: x in string.printable, text))
+    printable_text = printable_text.replace('\n', '')
+    return printable_text
+
+def divide_text_into_chunks(text, chunk_size=256):
+    # Create an empty list to hold the text chunks
+    text_chunks = []
+
+    # Create an empty list to hold the text chunks
+    text_chunks = []
+
+    # Split the text into words
+    words = text.split(' ')
+
+    # Loop through the words, adding chunks of the specified size to the list
+    for i in range(0, len(words), chunk_size):
+        word_chunk = words[i:i + chunk_size]
+        text_chunk = " ".join(word_chunk)
+        text_chunks.append(text_chunk)
+
+    # Return the list of text chunks
+    return text_chunks
+
+
 def main():
-    for pdf in tqdm(Path('/arxiv_data/EO_papers').rglob('*.pdf')):
-        reader = PyPDF2.PdfReader(str(pdf))
-        print(reader.metadata)
 
-        num_of_pages =  len(reader.pages)
-        print('Number of pages: ' + str(num_of_pages))
+    for pdf in tqdm(Path('/home/lcamilleri/git_repos/NLP4EO/arxiv_data/EO_papers/').rglob('*.pdf')):
 
-        text = convert_pdf_to_string(str(pdf))
-        text_2 = extract_text(str(pdf))
-        print()
+        text = extract_text(pdf)
+        if text != -1:
+            text = filter_nonprintable_characters(text)
+            text_chunks = divide_text_into_chunks(text)
+
+
+            dict = {"title": pdf.stem}
+            for i in range(len(text_chunks)):
+                dict[f"chunk_{i}"] = text_chunks[i]
+
+            with open(f"/home/lcamilleri/git_repos/NLP4EO/arxiv_data/EO_textdata/{pdf.stem}.json", "w") as fp:
+                json.dump(dict, fp)
 
 
 if __name__ == '__main__':
